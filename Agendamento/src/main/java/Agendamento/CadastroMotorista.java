fc722f1055d3eef9dddd9960f7edb4c449953d24
EltonEstevam <eltonestevam@live.com>
2024-03-10 11:58:54 -0300

    Versao 01.1

--- Agendamento/src/main/java/Agendamento/CadastroMotorista.java
+++ Agendamento/src/main/java/Agendamento/CadastroMotorista.java
@@ -4,19 +4,18 @@ import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
-import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
+
 public class CadastroMotorista {
 
-    private Set<String> matriculasCadastradas = new HashSet<>();
+    private Set<String> CNHsCadastradas = new HashSet<>();
 
     public void mostrarTelaCadastroMotorista() {
         Stage stage = new Stage();
@@ -29,67 +28,81 @@ public class CadastroMotorista {
         Label nomeLabel = new Label("Nome:");
         TextField nomeField = new TextField();
 
-        Label matriculaLabel = new Label("Matrícula:");
-        TextField matriculaField = new TextField();
+        Label CNHLabel = new Label("CNH:");
+        TextField CNHField = new TextField();
+        
+        Label ENDLabel = new Label("Endereço:");
+        TextField ENDField = new TextField();
 
-        Label dataNascimentoLabel = new Label("Data de Nascimento:");
-        DatePicker dataNascimentoPicker = new DatePicker();
-
+        Label TelefoneLabel = new Label("Telefone:");
+        TextField TelefoneField = new TextField();
+        
         grid.add(nomeLabel, 0, 0);
         grid.add(nomeField, 1, 0);
-        grid.add(matriculaLabel, 0, 1);
-        grid.add(matriculaField, 1, 1);
-        grid.add(dataNascimentoLabel, 0, 2);
-        grid.add(dataNascimentoPicker, 1, 2);
+        grid.add(CNHLabel, 0, 1);
+        grid.add(CNHField, 1, 1);
+        grid.add(ENDLabel, 0, 2);
+        grid.add(ENDField, 1, 2);
+        grid.add(TelefoneLabel, 0, 3);
+        grid.add(TelefoneField, 1, 3);
 
         Button cadastrarButton = new Button("Cadastrar");
         cadastrarButton.setOnAction(event -> {
-            if (validarCampos(nomeField.getText(), matriculaField.getText(), dataNascimentoPicker.getValue())) {
-                cadastroMotorista(nomeField.getText(), matriculaField.getText(), dataNascimentoPicker.getValue());
-                matriculasCadastradas.add(matriculaField.getText());
+            if (validarCampos(nomeField.getText(), CNHField.getText(), ENDField.getText(), TelefoneField.getText())) {
+                cadastroMotorista(nomeField.getText(), CNHField.getText(), ENDField.getText(), TelefoneField.getText());
+                CNHsCadastradas.add(CNHField.getText());
                 stage.close();
             }
         });
 
-        grid.add(cadastrarButton, 1, 3);
+        grid.add(cadastrarButton, 1, 5);
 
-        Scene scene = new Scene(grid, 300, 200);
+        Scene scene = new Scene(grid, 400, 300);
         stage.setScene(scene);
         stage.setTitle("Cadastro de Motorista");
         stage.show();
     }
+    
+    // VALIDAR OS CAMPOS PREENCHIDOS
 
-    private boolean validarCampos(String nome, String matricula, LocalDate dataNascimento) {
-        if (nome.isEmpty() || matricula.isEmpty() || dataNascimento == null) {
+    private boolean validarCampos(String nome, String CNH,  String END,String Telefone) {
+        if (nome.isEmpty() || CNH.isEmpty() || Telefone.isEmpty()) {
             exibirAlerta("Erro", "Todos os campos devem ser preenchidos.");
             return false;
         }
 
-        if (!nome.matches("[a-zA-Z]+")) {
+        if (!nome.matches("[a-z A-Z]+")) {
             exibirAlerta("Erro", "O campo 'Nome' deve conter apenas letras.");
             return false;
         }
 
-        if (!matricula.matches("[0-9]+")) {
-            exibirAlerta("Erro", "O campo 'Matrícula' deve conter apenas números.");
+        if (!CNH.matches("[0-9]+")) {
+            exibirAlerta("Erro", "O campo 'CNH' deve conter apenas números.");
             return false;
         }
 
-        if (matriculasCadastradas.contains(matricula)) {
-            exibirAlerta("Erro", "Matrícula já cadastrada.");
+        if (CNHsCadastradas.contains(CNH)) {
+            exibirAlerta("Erro", "CNH já cadastrada.");
             return false;
         }
 
         return true;
     }
-
-    private void cadastroMotorista(String nome, String matricula, LocalDate dataNascimento) {
+    
+    // SAIDA DAS INFORMAÇÕES NO CONSOLE
+    
+    private void cadastroMotorista(String nome, String CNH, String END, String Telefone) {
+    	
         System.out.println("Motorista cadastrado:");
         System.out.println("Nome: " + nome);
-        System.out.println("Matrícula: " + matricula);
-        System.out.println("Data de Nascimento: " + dataNascimento);
+        System.out.println("CNH: " + CNH);
+        System.out.println("Endereço: " + END);
+        System.out.println("Telefone: " + Telefone);
+    
     }
-
+     
+    // EXIBE MENSAGEM DE ERRO NA VALIDAÇÃO
+    
     private void exibirAlerta(String titulo, String conteudo) {
         Alert alert = new Alert(Alert.AlertType.ERROR);
         alert.setTitle(titulo);
